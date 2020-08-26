b45ab31542f3deedd9d3a8884297b8279d2b13f4
Jiten Patel <jitenpatel1013@gmail.com>
2020-08-25 20:02:10 -0500

    menu button

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,18 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class Controller {
+
+    public HBox cards;
+    public void addTask(ActionEvent actionEvent) {
+
+    }
+
+    public void addProject(ActionEvent actionEvent) {
+        VBox newProject = new VBox(new Label("Test"));
+        newProject.setId("project");
+        cards.getChildren().add(newProject);
+    }
 }
