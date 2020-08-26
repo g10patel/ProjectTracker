0ad98cb72d89230b7feb24036a4bf2bf676673b3
Jiten Patel <jitenpatel1013@gmail.com>
2020-08-26 10:35:00 -0500

    Gui creates new project and deletes if no project name is given

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,18 @@
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+
+import javax.swing.*;
 
 public class Controller {
 
@@ -11,8 +22,24 @@ public class Controller {
     }
 
     public void addProject(ActionEvent actionEvent) {
-        VBox newProject = new VBox(new Label("Test"));
+        VBox newProject = new VBox();
         newProject.setId("project");
+        TextField projectName = new TextField();
+
+        projectName.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if(keyEvent.getCode() == KeyCode.ENTER){
+                    if (projectName.getText().trim().isEmpty()){
+                        cards.getChildren().remove(newProject);
+                    }
+                }
+            }
+        });
+        projectName.setId("projectName");
+
+        projectName.setPromptText("Project Name");
+        newProject.getChildren().add(projectName);
         cards.getChildren().add(newProject);
     }
 }
