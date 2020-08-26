973a411039fd65c986056b494b3462098e9d1cf4
Jiten Patel <jitenpatel1013@gmail.com>
2020-08-26 13:55:07 -0500

    Move around project cards

--- src/Controller.java
+++ src/Controller.java
@@ -1,18 +1,25 @@
+import javafx.animation.FadeTransition;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-
+import javafx.scene.input.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 
 public class Controller {
 
@@ -25,7 +32,7 @@ public class Controller {
         VBox newProject = new VBox();
         newProject.setId("project");
         TextField projectName = new TextField();
-
+        final Label[] projectLabel = new Label[1];
         projectName.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent keyEvent) {
@@ -33,13 +40,49 @@ public class Controller {
                     if (projectName.getText().trim().isEmpty()){
                         cards.getChildren().remove(newProject);
                     }
+                    else{
+                        projectLabel[0] = new Label(projectName.getText());
+                        newProject.getChildren().remove(projectName);
+                        newProject.getChildren().add(projectLabel[0]);
+                        projectLabel[0].setId("projectName");
+                        Button left = new Button("left");
+                        Button right = new Button("right");
+                        HBox move = new HBox();
+                        left.setOnAction(e ->
+                        {
+                            ObservableList<Node> arr = FXCollections.observableArrayList(cards.getChildren());
+                            if(arr.indexOf(newProject) != 0) {
+                                Collections.swap(arr, arr.indexOf(newProject), arr.indexOf(newProject) - 1);
+                                cards.getChildren().clear();
+                                cards.getChildren().addAll(arr);
+
+                            }
+                        });
+
+                        right.setOnAction(e -> {
+                            ObservableList<Node> arr = FXCollections.observableArrayList(cards.getChildren());
+                            if(arr.indexOf(newProject) != arr.size()-1) {
+                                Collections.swap(arr, arr.indexOf(newProject), arr.indexOf(newProject) + 1);
+                                cards.getChildren().clear();
+                                cards.getChildren().addAll(arr);
+                            }
+                        });
+
+                        move.getChildren().addAll(left,right);
+                        newProject.getChildren().add(move);
+
+                    }
                 }
             }
         });
-        projectName.setId("projectName");
 
+        projectName.setId("projectName");
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(.05), newProject);
+        fadeTransition.setFromValue(0.0);
+        fadeTransition.setToValue(1.0);
         projectName.setPromptText("Project Name");
         newProject.getChildren().add(projectName);
         cards.getChildren().add(newProject);
+        fadeTransition.play();
     }
 }
